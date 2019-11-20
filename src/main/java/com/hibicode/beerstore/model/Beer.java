1cf0fa7dd384da4ca6dd31f50641657b71fcf392
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-11-20 08:27:41 -0300

    Tratando erros de forma centralizada

--- src/main/java/com/hibicode/beerstore/model/Beer.java
+++ src/main/java/com/hibicode/beerstore/model/Beer.java
@@ -19,11 +19,11 @@ public class Beer {
     @SequenceGenerator(name = "beer_seq", sequenceName = "beer_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "beer_seq")
     private Long id;
-    @NotBlank
+    @NotBlank(message = "beers-1")
     private String name;
-    @NotNull
+    @NotNull(message = "beers-2")
     private BeerType type;
-    @NotNull
-    @DecimalMin("0")
+    @NotNull(message = "beers-3")
+    @DecimalMin(value = "0", message = "beers-4")
     private BigDecimal volume;
 }
