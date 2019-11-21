9d617b97bae147202661d646b3121993c306601f
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-11-21 20:21:30 -0300

    Tratando bug nas exceções centralizadas

--- src/main/java/com/hibicode/beerstore/error/ApiExceptionHandler.java
+++ src/main/java/com/hibicode/beerstore/error/ApiExceptionHandler.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -23,6 +25,7 @@ import static java.util.stream.Collectors.toList;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ApiExceptionHandler {
 
     private static final String NO_MESSAGE_AVAILABLE = "No message available";
@@ -52,17 +55,7 @@ public class ApiExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception exception, Locale locale) {
-        LOG.error("Error not expected", exception);
-        final String errorCode = "error-1";
-        final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        final ErrorResponse errorResponse = ErrorResponse.of(status, toApiError(errorCode, locale));
-
-        return ResponseEntity.status(status).body(errorResponse);
-    }
-
-    private ApiError toApiError(String code, Locale locale, Object... args) {
+    public ApiError toApiError(String code, Locale locale, Object... args) {
         String message;
         try {
             message = apiErrorMessageSource.getMessage(code, args, locale);
